74b256caf8cce48e97f110a8ae1b55bdeeec9c09
OrbitCo <40737384+OrbitCo@users.noreply.github.com>
2021-07-03 15:53:09 -0500

    Update app-service-main.tf

--- modules/node/app-service-main.tf
+++ modules/node/app-service-main.tf
@@ -8,7 +8,7 @@ resource "azurerm_app_service" "appservice12" {
   name                = "appserviceTwo"
   location            = "Central US"
   resource_group_name = "mliadov-01"
- app_service_plan_id = azurerm_app_service_plan.service-plan.id
+ app_service_plan_id = azurerm_app_service_plan.cosmosnode.id
 
 
 }
