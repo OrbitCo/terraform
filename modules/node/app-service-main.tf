5b675891e061b3502e337772e2e9ccf8ad64f106
OrbitCo <40737384+OrbitCo@users.noreply.github.com>
2021-07-03 16:06:31 -0500

    Update app-service-main.tf

--- modules/node/app-service-main.tf
+++ modules/node/app-service-main.tf
@@ -1,14 +1,32 @@
 ##############################
 ## Azure App Service - Main ##
 ##############################
+# Create the App Service Plan
+resource "azurerm_app_service_plan" "service-plan" {
+  name                = "ASP-mliadov01-98e6 (P1v2: 1)"
+  location            = azurerm_resource_group.appservice-rg.location
+  resource_group_name = azurerm_resource_group.appservice-rg.name
+  kind                = "Linux"
+  reserved            = true
 
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+
+  tags = {
+    description = var.description
+    environment = var.environment
+    owner       = var.owner  
+  }
+}
 
 # Create the App Service
 resource "azurerm_app_service" "appservice12" {
   name                = "appserviceTwo"
   location            = "Central US"
   resource_group_name = "mliadov-01"
- app_service_plan_id = "e480a2a1-a59c-4b77-aa21-ffcbc8243f5f"
+ app_service_plan_id = azurerm_app_service_plan.service-plan.id
 
 
 }
