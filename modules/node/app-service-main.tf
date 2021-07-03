43789f23514045a1527d91812c2efbf236d54b24
OrbitCo <40737384+OrbitCo@users.noreply.github.com>
2021-07-03 14:40:55 -0500

    Update app-service-main.tf

--- modules/node/app-service-main.tf
+++ modules/node/app-service-main.tf
@@ -44,10 +44,7 @@ resource "azurerm_app_service" "app-service" {
   site_config {
     linux_fx_version = "DOTNETCORE|3.1"
   }
-source_control {
-  repourl = $${var.url}
-  branch = $${var.branch}
-  }
+
   
   tags = {
     description = var.description
