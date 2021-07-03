9934cbd44e4895aad3f9f0087d7bf8485f93db97
OrbitCo <40737384+OrbitCo@users.noreply.github.com>
2021-07-03 15:30:09 -0500

    Update app-service-main.tf

--- modules/node/app-service-main.tf
+++ modules/node/app-service-main.tf
@@ -2,57 +2,13 @@
 ## Azure App Service - Main ##
 ##############################
 
-# Create a Resource Group
-resource "azurerm_resource_group" "mliadov-01" {
-  name     = "${var.region}-${var.environment}-${var.app_name}-app-service-rg"
-  location = var.location
-
-  tags = {
-    description = var.description
-    environment = var.environment
-    owner       = var.owner  
-  }
-}
-
-# Create the App Service Plan
-resource "azurerm_app_service_plan" "service-plan" {
-  name                = "${var.region}-${var.environment}-${var.app_name}-service-plan"
-  location            = azurerm_resource_group.mliadov-01.location
-  resource_group_name = azurerm_resource_group.mliadov-01.name
-  kind                = "Linux"
-  reserved            = true
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-
-  tags = {
-    description = var.description
-    environment = var.environment
-    owner       = var.owner  
-  }
-}
 
 # Create the App Service
 resource "azurerm_app_service" "app-service" {
-  name                = "${var.region}-${var.environment}-${var.app_name}-app-service"
+  name                = "appserviceTwo"
   location            = azurerm_resource_group.mliadov-01.location
   resource_group_name = azurerm_resource_group.mliadov-01.name
   app_service_plan_id = azurerm_app_service_plan.service-plan.id
 
-  site_config {
-    linux_fx_version = "DOTNETCORE|3.1"
-  }
 
-  source_control {
-    repo_url = "https://github.com/OrbitCo/terraform.git"
-    branch = "master"
-  }
-  
-  tags = {
-    description = var.description
-    environment = var.environment
-    owner       = var.owner  
-  }
 }
