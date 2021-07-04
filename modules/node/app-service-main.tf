ec3cdf8d0c6aafd6282882ae954f33c87108602c
OrbitCo <40737384+OrbitCo@users.noreply.github.com>
2021-07-03 20:31:47 -0500

    Update app-service-main.tf

--- modules/node/app-service-main.tf
+++ modules/node/app-service-main.tf
@@ -22,8 +22,8 @@ resource "azurerm_app_service_plan" "service-plan" {
 }
 
 # Create the App Service
-resource "azurerm_app_service" "appservice308" {
-  name                = "appservice308"
+resource "azurerm_app_service" "appservice309" {
+  name                = "appservice309"
   location            = "Central US"
   resource_group_name = "mliadov-01"
  app_service_plan_id = azurerm_app_service_plan.service-plan.id
